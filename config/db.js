ee3ed8bf405428c9d2a9a3add5e8349a2f5dc2f8
Zaryu <adityathaakur081@gmail.com>
2026-04-01 23:04:40 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
     await mongoose.connect(
-      "mongodb+srv://adityaathakur081_db_user:<db_Password009@>@cluster0.tekm9oo.mongodb.net/"
+      "mongodb+srv://adityaathakur081_db_user:<db_Password009@a>@cluster0.tekm9oo.mongodb.net/"
     );
     console.log("MongoDB Connected ✅");
   } catch (error) {
